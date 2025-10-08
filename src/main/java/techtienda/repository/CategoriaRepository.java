ff5aea86cbf92e1209918898c232bf8621cd7257
Jimena Marin <jime.marin@icloud.com>
2025-10-07 22:57:26 -0600

    Commit semana 4 -5
    Hubo problemas tratando correr el proyecto debido a que la carpeta de GitHub estaba ligada a OneDrive.
    
    Se retrocedio una version y quedan pendientes algunos cambios de semana 5

---
+++ src/main/java/techtienda/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package techtienda.repository;
+
+import techtienda.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+
+}
